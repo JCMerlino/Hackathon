e13a0ff1176450fc78762b43d43b726b1dccfc3d
sadonEmsi <konstantinos.tsolakakis@gmail.com>
2017-10-21 22:17:15 +0100

    listings and events

---
+++ events.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'events.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(1111, 860)
+        MainWindow.setLayoutDirection(QtCore.Qt.RightToLeft)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.label = QtGui.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(0, -10, 1281, 141))
+        self.label.setText(_fromUtf8(""))
+        self.label.setPixmap(QtGui.QPixmap(_fromUtf8("banner.png")))
+        self.label.setObjectName(_fromUtf8("label"))
+        self.pushButton = QtGui.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(420, 700, 211, 51))
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.frame = QtGui.QFrame(self.centralwidget)
+        self.frame.setGeometry(QtCore.QRect(30, 260, 1061, 141))
+        self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtGui.QFrame.Raised)
+        self.frame.setObjectName(_fromUtf8("frame"))
+        self.label_2 = QtGui.QLabel(self.frame)
+        self.label_2.setGeometry(QtCore.QRect(12, 15, 101, 111))
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.lineEdit = QtGui.QLineEdit(self.frame)
+        self.lineEdit.setGeometry(QtCore.QRect(170, 20, 791, 101))
+        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
+        self.pushButton_3 = QtGui.QPushButton(self.frame)
+        self.pushButton_3.setGeometry(QtCore.QRect(970, 50, 81, 28))
+        self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
+        self.frame_2 = QtGui.QFrame(self.centralwidget)
+        self.frame_2.setGeometry(QtCore.QRect(30, 470, 1061, 131))
+        self.frame_2.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QtGui.QFrame.Raised)
+        self.frame_2.setObjectName(_fromUtf8("frame_2"))
+        self.label_3 = QtGui.QLabel(self.frame_2)
+        self.label_3.setGeometry(QtCore.QRect(12, 15, 101, 101))
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.lineEdit_2 = QtGui.QLineEdit(self.frame_2)
+        self.lineEdit_2.setGeometry(QtCore.QRect(170, 21, 791, 91))
+        self.lineEdit_2.setObjectName(_fromUtf8("lineEdit_2"))
+        self.pushButton_5 = QtGui.QPushButton(self.frame_2)
+        self.pushButton_5.setGeometry(QtCore.QRect(970, 50, 81, 28))
+        self.pushButton_5.setObjectName(_fromUtf8("pushButton_5"))
+        self.label_4 = QtGui.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(500, 160, 191, 81))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.label_4.setFont(font)
+        self.label_4.setLayoutDirection(QtCore.Qt.RightToLeft)
+        self.label_4.setTextFormat(QtCore.Qt.RichText)
+        self.label_4.setObjectName(_fromUtf8("label_4"))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1111, 26))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        self.menuProfile = QtGui.QMenu(self.menubar)
+        self.menuProfile.setLayoutDirection(QtCore.Qt.RightToLeft)
+        self.menuProfile.setObjectName(_fromUtf8("menuProfile"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionView_your_profile = QtGui.QAction(MainWindow)
+        self.actionView_your_profile.setObjectName(_fromUtf8("actionView_your_profile"))
+        self.actionSettings = QtGui.QAction(MainWindow)
+        self.actionSettings.setObjectName(_fromUtf8("actionSettings"))
+        self.actionLog_out = QtGui.QAction(MainWindow)
+        self.actionLog_out.setObjectName(_fromUtf8("actionLog_out"))
+        self.menuProfile.addAction(self.actionView_your_profile)
+        self.menuProfile.addAction(self.actionSettings)
+        self.menuProfile.addAction(self.actionLog_out)
+        self.menubar.addAction(self.menuProfile.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.pushButton.setText(_translate("MainWindow", "Back to the hub.", None))
+        self.label_2.setText(_translate("MainWindow", "picture1", None))
+        self.pushButton_3.setText(_translate("MainWindow", "Delete", None))
+        self.label_3.setText(_translate("MainWindow", "picture2", None))
+        self.pushButton_5.setText(_translate("MainWindow", "Delete", None))
+        self.label_4.setText(_translate("MainWindow", "Your Events", None))
+        self.menuProfile.setTitle(_translate("MainWindow", "Profile", None))
+        self.actionView_your_profile.setText(_translate("MainWindow", "View your profile", None))
+        self.actionSettings.setText(_translate("MainWindow", "Settings", None))
+        self.actionLog_out.setText(_translate("MainWindow", "Log out", None))
+
---
+++ post_a_listing.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'post_a_listing.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(1111, 860)
+        MainWindow.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.label = QtGui.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(0, 0, 1281, 141))
+        self.label.setText(_fromUtf8(""))
+        self.label.setPixmap(QtGui.QPixmap(_fromUtf8("banner.png")))
+        self.label.setObjectName(_fromUtf8("label"))
+        self.label_2 = QtGui.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(120, 250, 161, 31))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.label_3 = QtGui.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(200, 320, 81, 31))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.dateTimeEdit = QtGui.QDateTimeEdit(self.centralwidget)
+        self.dateTimeEdit.setGeometry(QtCore.QRect(300, 320, 251, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.dateTimeEdit.setFont(font)
+        self.dateTimeEdit.setObjectName(_fromUtf8("dateTimeEdit"))
+        self.label_4 = QtGui.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(120, 380, 151, 31))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.label_4.setFont(font)
+        self.label_4.setObjectName(_fromUtf8("label_4"))
+        self.plainTextEdit = QtGui.QPlainTextEdit(self.centralwidget)
+        self.plainTextEdit.setGeometry(QtCore.QRect(300, 400, 381, 231))
+        self.plainTextEdit.setObjectName(_fromUtf8("plainTextEdit"))
+        self.plainTextEdit_2 = QtGui.QPlainTextEdit(self.centralwidget)
+        self.plainTextEdit_2.setGeometry(QtCore.QRect(280, 250, 401, 31))
+        self.plainTextEdit_2.setObjectName(_fromUtf8("plainTextEdit_2"))
+        self.pushButton = QtGui.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(580, 660, 101, 41))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.pushButton_2 = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(400, 730, 191, 51))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1111, 26))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        self.menuProfile = QtGui.QMenu(self.menubar)
+        self.menuProfile.setLayoutDirection(QtCore.Qt.RightToLeft)
+        self.menuProfile.setObjectName(_fromUtf8("menuProfile"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionView_your_profile = QtGui.QAction(MainWindow)
+        self.actionView_your_profile.setObjectName(_fromUtf8("actionView_your_profile"))
+        self.actionSettings = QtGui.QAction(MainWindow)
+        self.actionSettings.setObjectName(_fromUtf8("actionSettings"))
+        self.actionLog_out = QtGui.QAction(MainWindow)
+        self.actionLog_out.setObjectName(_fromUtf8("actionLog_out"))
+        self.menuProfile.addAction(self.actionView_your_profile)
+        self.menuProfile.addAction(self.actionSettings)
+        self.menuProfile.addAction(self.actionLog_out)
+        self.menubar.addAction(self.menuProfile.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.label_2.setText(_translate("MainWindow", "Listing Title:", None))
+        self.label_3.setText(_translate("MainWindow", "Date:", None))
+        self.label_4.setText(_translate("MainWindow", "Description:", None))
+        self.pushButton.setText(_translate("MainWindow", "Post", None))
+        self.pushButton_2.setText(_translate("MainWindow", "Back to the hub", None))
+        self.menuProfile.setTitle(_translate("MainWindow", "Profile", None))
+        self.actionView_your_profile.setText(_translate("MainWindow", "View your profile", None))
+        self.actionSettings.setText(_translate("MainWindow", "Settings", None))
+        self.actionLog_out.setText(_translate("MainWindow", "Log out", None))
+
