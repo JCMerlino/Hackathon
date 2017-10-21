88fc0274cdc0b9900fc4bdf794d4d232d7819613
finleylewis4 <flewis553@gmail.com>
2017-10-21 23:36:56 +0100

    Templated database and made username verification

--- form_template.py
+++ form_template.py
@@ -1,9 +1,31 @@
-form = {
-	"name": "",
-	"location": "",
-	"age": 0,
-	"gender": "",
-	"email": "",
-	"password": "",
-	"msg_link": ""
-	}
+#TEMPLATE USER
+#-add users to main dict using on signup inputs
+#-add events to event dict using "make listing" inputs
+#-login checks username['password'] against password input
+from security import *
+users = {
+	username: {"name": "",
+                   "location": "",
+	           "age": 0,
+	           "gender": "",
+	           "email": "",
+	           "password": "", #hashed
+	           "msg_link": ""
+                   "events" : { "event1" :
+                       {"time" : "00:00",
+                        "description" : ""
+                   }
+        }
+                
+def login_verif(user,pword):
+        
+"""
+simple check if password is correct,
+if password is wrong print an error and clear inputs
+"""
+        enc_pword=encrypt(pword)
+        if enc_pword==user['password']:
+            return True
+        else:
+            return False
+        
