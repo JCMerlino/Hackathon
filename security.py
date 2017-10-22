a62bec7cba7012ebb21e72bb174a823070a9939a
finleylewis4 <flewis553@gmail.com>
2017-10-22 01:06:36 +0100

    changes in workings

--- security.py
+++ security.py
@@ -6,29 +6,24 @@ def encrypt(password):
 
     xor_cipher = "00011011"
     encrypted_password = ""
-    print(numberlist)
     i = -1
-    for i in range:
-        for ch in numberlist:
-            i += 1
-            if i > 7:
-                i = 0
-                
-            if ch == "1":
-                if xor_cipher[i] == "1":
-                    ch = 0
-                else:
-                    ch = 1
-            elif ch == "0":
-                if xor_cipher[i] == "0":
-                    ch = 0
-                else:
-                    ch = 1
+    for ch in numberlist:
+        i += 1
+        if i > 7:
+            i = 0
+            
+        if ch == "1":
+            if xor_cipher[i] == "1":
+                ch = 0
+            else:
+                ch = 1
+        elif ch == "0":
+            if xor_cipher[i] == "0":
+                ch = 0
+            else:
+                ch = 1
 
-            encrypted_password += str(ch)
+        encrypted_password += str(ch)
 
 
     return encrypted_password
-
-
-
